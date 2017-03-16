ae84374744ae533da721a92f75e664a8d4167081
dtw8hn <dtw8hn@virginia.edu>
2017-03-15 23:22:13 -0400

    Seperate

---
+++ Full.java
@@ -0,0 +1,12 @@
+
+public class Full {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Game screen1 = new Game(null, 1);
+		//Game screen2 = new Game(null, 2);
+		//Game screen3 = new Game(null, 3);
+		//Game screen4 = new Game(null, 4);
+	}
+
+}
---
+++ In_game_screen.java
@@ -0,0 +1,15 @@
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+
+
+public class In_game_screen extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public In_game_screen() {
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+	}
+
+}
